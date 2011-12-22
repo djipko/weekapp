41344ffde0eb93359dda64666c6179326988f100
Nikola Dipanov <nikola.djipanov@gmail.com>
2011-12-22 18:56:57 +0100

    The initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weekapp::Application.config.secret_token = 'f8378552656ed3b5a864a080cb2473a2f6e7cd81d31c68741c96f9a937e32d1bdf0ad8a3264f636fdbddef11b319dfe68e7cf9ebfa9d50e20db1d9f7d3e1ef95'
