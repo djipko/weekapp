bfea7e46abfb71c50f9632bf437ce8f52103bbde
Nikola Dipanov <nikola.djipanov@gmail.com>
2011-12-25 17:53:05 +0100

    Comments are now added with AJAX === *Clicking the comment button causes the comment to be added by Ajax and the form cleared. *Improvements could include validating data, and adding the time of comment. *Took me some time to figure out the rails way to do ajax - super simple - thumbs up!!!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,9 @@ class CommentsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
     @comment = @event.comments.create(params[:comment])
-    redirect_to event_path(@event) #for now we redirect - later we will Ajax this bad boy
+    respond_to do |format|
+        format.html { redirect_to event_path(@event) } #for now we redirect - later we will Ajax this bad boy
+        format.js
+    end
   end
 end
